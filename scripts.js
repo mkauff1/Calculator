ccc7b9cd5540ba155b6641484dff6a1f92899340
Matthew Kauff <matthew.kauff@live.com>
2021-03-30 20:40:30 -0400

    Added Clear Button

--- scripts.js
+++ scripts.js
@@ -16,13 +16,119 @@ function getValidNumber(message) {
 //var c = Math.sqrt((a*a)+(b*b));
 //alert("Hello " + c);
 
-function enterClick() {
-    alert("You clicked me");
+function PerformOp(operation) {
+    firstValue = outputBox.innerHTML;
+    outputBox.innerHTML = "0";
+    operationPerf = operation;
 }
 
+function divideClick() {
+    PerformOp("/");
+}
+function multiplyClick() {
+    PerformOp("*");
+}
+function subtractClick() {
+    PerformOp("-");
+}
+function additionClick() {
+    PerformOp("+");
+}
+function sevenClick() {
+    addToOutputBox("7");
+}
+function eightClick() {
+    addToOutputBox("8");
+}
+function nineClick() {
+    addToOutputBox("9");
+}
+function fourClick() {
+    addToOutputBox("4");
+}
+function fiveClick() {
+    addToOutputBox("5");
+}
+function sixClick() {
+    addToOutputBox("6");
+}
+function oneClick() {
+     addToOutputBox("1");
+}
+function twoClick() {
+    addToOutputBox("2");
+}
+function threeClick() {
+    addToOutputBox("3");
+}
+function clearClick() {
+    PerformOp("");
+}
+function zeroClick() {
+    addToOutputBox("0");
+}
+function periodClick() {
+    addToOutputBox(".");
+}
+function enterClick() {
+    var a = parseFloat(firstValue);
+    var b = parseFloat(outputBox.innerHTML);
+    var c = 0;
+
+    switch (operationPerf) {
+        case "+":
+            c = a + b; 
+            break;
+
+        case "-":
+            c = a - b;
+            break;
+
+        case "*":
+            c = a * b;
+            break;
+
+        case "/":
+            c = a / b;
+            break;
+    }
+
+    outputBox.innerHTML = c.toString();
+}
+
+var outputBox;
+var firstValue;
+var operationPerf;
+
+
+function addToOutputBox(number){
+    
+    if (outputBox.innerHTML === "0"){
+        outputBox.innerHTML = number;
+    }
+    else {
+        outputBox.innerHTML += number;
+    }
+}
 function loaded() {
-    var enterBtn = document.getElementById("enter-btn");
-    enterBtn.addEventListener("click", enterClick);
+    outputBox = document.getElementById("output-box");
+    document.getElementById("divide-btn").addEventListener("click", divideClick);
+    document.getElementById("multiply-btn").addEventListener("click", multiplyClick);
+    document.getElementById("subtract-btn").addEventListener("click", subtractClick);
+    document.getElementById("addition-btn").addEventListener("click", additionClick);
+    document.getElementById("seven-btn").addEventListener("click", sevenClick);
+    document.getElementById("eight-btn").addEventListener("click", eightClick);
+    document.getElementById("nine-btn").addEventListener("click", nineClick);
+    document.getElementById("four-btn").addEventListener("click", fourClick);
+    document.getElementById("five-btn").addEventListener("click", fiveClick);
+    document.getElementById("six-btn").addEventListener("click", sixClick);
+    document.getElementById("one-btn").addEventListener("click", oneClick);
+    document.getElementById("two-btn").addEventListener("click", twoClick);
+    document.getElementById("three-btn").addEventListener("click", threeClick);
+    document.getElementById("clear-btn").addEventListener("click", clearClick);
+    document.getElementById("zero-btn").addEventListener("click", zeroClick);
+    document.getElementById("period-btn").addEventListener("click", periodClick);
+    document.getElementById("enter-btn").addEventListener("click", enterClick);
 }
 
 window.addEventListener("load", loaded);
